049df458b6e0cbf6898d36a9cf155761c3dedb72
Robbi Wafik <robbiwafik31@gmail.com>
2023-04-02 17:48:41 +0700

    Define color pallete

---
+++ app/assets/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    danger: '#DC4C64',
+    dark: '#6F7780',
+    darkPrimary: '#159895',
+    light: '#C2C8D0',
+    primary: '#57C5B6',
+    secondary: '#1A5F7A',
+    secondaryDark: '#002B5B',
+    white: '#FFF',
+}
+
+export default colors;
