6dca4142bc25bca2aba9d23df05ba9f746101a8e
Robbi Wafik <robbiwafik31@gmail.com>
2023-05-01 06:19:10 +0700

    Complete the build of AppDocumentInput component that was accidentally pushed to the repository

--- app/components/AppDocumentInput.jsx
+++ app/components/AppDocumentInput.jsx
@@ -7,24 +7,33 @@ import colors from '../assets/colors';
 import defaultStyles from '../assets/defaultStyles';
 import ErrorMessage from './ErrorMessage';
 
-export default function AppDocumentInput({ onPickFile, error }) {
+export default function AppDocumentInput({ onSelectFile, error, label }) {
     const [fileName, setFileName] = useState();
 
-    const pickFile = async () => {
+    const handlePickFile = async () => {
         const pickedFile = await DocumentPicker.getDocumentAsync();
         setFileName(pickedFile.name);
-        onPickFile(pickedFile);
+        onSelectFile(pickedFile);
     }
 
     return (
         <>
-            <TouchableWithoutFeedback onPress={pickFile}>
-                <View style={error ? [styles.filePicker, {borderColor: colors.danger}] : styles.filePicker}>
-                    <FontAwesome name='file-text' size={30} color={error ? colors.danger : colors.light} />
-                    <Text numberOfLines={1} style={styles.fileName}>{fileName ? fileName : 'Pilih File'}</Text>
+            <TouchableWithoutFeedback onPress={handlePickFile}>
+                <View style={error ? [styles.filePicker, styles.error] : styles.filePicker}>
+                    <FontAwesome 
+                        color={error ? colors.danger : colors.light} 
+                        name='file-text' 
+                        size={30} 
+                    />
+                    <Text 
+                        numberOfLines={1} 
+                        style={error ? [styles.fileName, styles.error] : styles.fileName}
+                    >
+                        {fileName ? fileName : label}
+                    </Text>
                 </View>
             </TouchableWithoutFeedback>
-            <ErrorMessage error={error} />
+            <ErrorMessage error={error} visible={true} />
         </>
     );
 }
@@ -44,5 +53,9 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         marginVertical: defaultStyles.verticalSpace,
         padding: 16
+    },
+    error: {
+        borderColor: colors.danger,
+        color: colors.danger
     }
 });
